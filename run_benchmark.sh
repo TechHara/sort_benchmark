c002ca265c520531d43eeb4e0ad55a759738c173
TechHara <techhara@outlook.com>
2023-02-06 11:56:35 -0500

    read at once

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,3 +1,5 @@
+LC_ALL=C
+
 echo sort
 for n in `seq 10`; do perf stat -e instructions sort --parallel 1 -o output input_$n.tsv 2>&1 | grep instructions | gawk '{print $1}' ; done
 for n in `seq 10`; do /usr/bin/time -v sort --parallel 1 -o output input_$n.tsv 2>&1 | grep wall | gawk '{print $8}' | cut -f2 -d: ; done
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,27 @@
 use std::env::args;
-use std::io::{self, BufReader, BufRead, BufWriter, Write};
 use std::fs::File;
+use std::io::{self, BufReader, BufWriter, Read, Write};
 
 fn main() -> io::Result<()> {
     let params: Vec<_> = args().collect();
     let input_file = if params.len() < 2 || params[1] == "-" {
         "/dev/stdin".to_owned()
-    } else { params[1].clone() };
+    } else {
+        params[1].clone()
+    };
     let output_file = if params.len() < 3 || params[2] == "-" {
         "/dev/stdout".to_owned()
-    } else { params[2].clone() };
-    let ifs = BufReader::new(File::open(input_file)?);
+    } else {
+        params[2].clone()
+    };
+    let mut ifs = BufReader::new(File::open(input_file)?);
+    let mut buf = String::new();
+    ifs.read_to_string(&mut buf)?;
+
     let mut ofs = BufWriter::new(File::create(output_file)?);
-    
+
     let mut lines = Vec::new();
-    for line in ifs.lines() {
-        let line = line?;
+    for line in buf.split_inclusive('\n') {
         lines.push(line);
     }
 
